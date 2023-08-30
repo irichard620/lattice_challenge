9e3eb82f856273d4d3907025a51edd715bb1d6f3
Ian Richard <ianrich620@gmail.com>
2023-08-30 16:19:01 -0700

    Test commit 2

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cors = require("cors");
 require('dotenv').config()
 
-// Interesting thing here
+// Interesting thing here change
 var indexRouter = require('./routes/index');
 var moviesRouter = require("./routes/movies");
 var searchRouter = require("./routes/search");
